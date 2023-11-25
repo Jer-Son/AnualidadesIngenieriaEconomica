94220fa157cfb626cd5b3c19d2fc6b2d070c39a7
Jer-Son <jersonhenao30@gmail.com>
2023-11-25 17:54:50 -0500

    TablaOrdinaryPresentOk

--- app.js
+++ app.js
@@ -5,59 +5,89 @@ document.addEventListener('DOMContentLoaded', function() {
 function handleFormSubmit(event) {
     event.preventDefault();
     
-    const presentValue = parseFloat(document.getElementById('presentValue').value);
-    const futureValue = parseFloat(document.getElementById('futureValue').value);
-    const interestRate = parseFloat(document.getElementById('interestRate').value) / 100; // Convertir a tasa decimal
-    const periods = parseInt(document.getElementById('periods').value);
+    const presentValueInput = document.getElementById('presentValue').value;
+    const futureValueInput = document.getElementById('futureValue').value;
+    const interestRateInput = document.getElementById('interestRate').value;
+    const periodsInput = document.getElementById('periods').value;
     const annuityType = document.getElementById('annuityType').value;
     
-    if (isNaN(presentValue) || isNaN(interestRate) || isNaN(periods)) {
-        displayError('All inputs must be numeric values');
+    if (!validateInput(interestRateInput, 'Interest Rate') || 
+        !validateInput(periodsInput, 'Number of Periods')) {
         return;
     }
     
-    let result;
-    if (annuityType === 'ordinary') {
-        result = calculateOrdinaryAnnuity(presentValue, interestRate, periods);
-    } else if (annuityType === 'due') {
-        result = calculateAnnuityDue(presentValue, interestRate, periods);
+    const interestRate = parseFloat(interestRateInput);
+    const periods = parseInt(periodsInput);
+
+    let presentValue = parseFloat(presentValueInput) || 0;
+    let futureValue = parseFloat(futureValueInput) || 0;
+    
+    let paymentAmount;
+
+    if(presentValue === 0 && futureValue !== 0) {
+        // Calcular el valor presente a partir del valor futuro
+        presentValue = calculatePresentValue(futureValue, interestRate, periods);
+        paymentAmount = calculatePaymentAmount(presentValue, interestRate, periods, annuityType);
+    } else if(presentValue !== 0 && futureValue === 0) {
+        // Calcular el valor futuro a partir del valor presente
+        paymentAmount = calculatePaymentAmount(presentValue, interestRate, periods, annuityType);
+        futureValue = calculateFutureValue(paymentAmount, interestRate, periods, annuityType);
     } else {
-        displayError('Invalid annuity type');
+        displayError('Please enter either a present value or a future value, not both.');
         return;
     }
-    
-    if (result === null) {
-        displayError('Calculation error: invalid inputs');
-    } else {
-        displayResult(result, presentValue, interestRate, periods);
+
+    const totalInterest = calculateTotalInterest(paymentAmount, periods, presentValue);
+
+    displayResult(paymentAmount, presentValue, futureValue, totalInterest, interestRate, periods, annuityType);
+    generateAmortizationTable(paymentAmount, interestRate, periods, presentValue);
+}
+
+function calculatePresentValue(futureValue, interestRate, periods) {
+    const interestRateDecimal = interestRate / 100;
+    return futureValue / Math.pow(1 + interestRateDecimal, periods);
+}
+
+function calculatePaymentAmount(presentValue, interestRate, periods, annuityType) {
+    const interestRateDecimal = interestRate / 100;
+    const annuityFactor = (1 - Math.pow(1 + interestRateDecimal, -periods)) / interestRateDecimal;
+    const paymentAmount = presentValue / annuityFactor;
+
+    return annuityType === 'ordinary' ? paymentAmount : paymentAmount * (1 + interestRateDecimal);
+}
+
+function calculateFutureValue(paymentAmount, interestRate, periods, annuityType) {
+    const interestRateDecimal = interestRate / 100;
+    const futureValue = paymentAmount * ((Math.pow(1 + interestRateDecimal, periods) - 1) / interestRateDecimal);
+
+    return annuityType === 'ordinary' ? futureValue : futureValue * (1 + interestRateDecimal);
+}
+
+function validateInput(value, fieldName) {
+    if (isNaN(value) || value < 0) {
+        displayError(`Invalid ${fieldName}. Please enter a positive number or zero.`);
+        return false;
     }
+    return true;
 }
 
-function calculateOrdinaryAnnuity(presentValue, interestRate, periods) {
-    const paymentAmount = presentValue * (interestRate / (1 - Math.pow(1 + interestRate, -periods)));
-    return {
-        presentValue: presentValue,
-        futureValue: paymentAmount * periods,
-        paymentAmount: paymentAmount
-    };
+function calculateTotalInterest(paymentAmount, periods, presentValue) {
+    const totalPaymentAmount = paymentAmount * periods;
+    const totalInterest = totalPaymentAmount - presentValue;
+    return totalInterest.toFixed(2);
 }
 
-function calculateAnnuityDue(presentValue, interestRate, periods) {
-    const paymentAmount = presentValue * (interestRate / (1 - Math.pow(1 + interestRate, -periods))) * (1 + interestRate);
-    return {
-        presentValue: presentValue,
-        futureValue: paymentAmount * periods,
-        paymentAmount: paymentAmount
-    };
+function displayError(errorMessage) {
+    document.getElementById('result').innerHTML = `Error: ${errorMessage}`;
 }
 
-function displayResult(result, presentValue, interestRate, periods) {
+function displayResult(paymentAmount, presentValue, futureValue, totalInterest, interestRate, periods, annuityType) {
     document.getElementById('result').innerHTML = `
-        Present Value: ${result.presentValue}<br>
-        Future Value: ${result.futureValue}<br>
-        Payment Amount: ${result.paymentAmount}
+        Present Value: ${formatCurrency(presentValue)}<br>
+        Future Value: ${formatCurrency(futureValue)}<br>
+        Payment Amount: ${formatCurrency(paymentAmount)}<br>
+        Total Interest: ${formatCurrency(totalInterest)}
     `;
-    generateAmortizationTable(result.paymentAmount, interestRate, periods, presentValue);
 }
 
 function generateAmortizationTable(paymentAmount, interestRate, periods, presentValue) {
@@ -67,18 +97,18 @@ function generateAmortizationTable(paymentAmount, interestRate, periods, present
     document.getElementById('amortizationTable').getElementsByTagName('tbody')[0].innerHTML = '';
 
     for (let period = 1; period <= periods; period++) {
-        let interest = balance * interestRate;
+        let interest = balance * (interestRate / 100);
         let principal = paymentAmount - interest;
         balance -= principal;
 
         tableBody += `
             <tr>
                 <td>${period}</td>
-                <td>${(balance + principal)}</td>
-                <td>${paymentAmount}</td>
-                <td>${interest}</td>
-                <td>${principal}</td>
-                <td>${(balance > 0 ? balance : 0)}</td>
+                <td>${formatCurrency(balance + principal)}</td>
+                <td>${formatCurrency(paymentAmount)}</td>
+                <td>${formatCurrency(interest)}</td>
+                <td>${formatCurrency(principal)}</td>
+                <td>${formatCurrency(balance)}</td>
             </tr>
         `;
     }
@@ -87,6 +117,6 @@ function generateAmortizationTable(paymentAmount, interestRate, periods, present
     document.getElementById('tableContainer').style.display = 'block';
 }
 
-function displayError(message) {
-    document.getElementById('result').innerHTML = `Error: ${message}`;
+function formatCurrency(value) {
+    return '$' + Number(value).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 }
